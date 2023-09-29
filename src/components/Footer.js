21a234218f0ea72672319ea16375342463641fb6
Ethan <ethan@s4654700.vpn.uq.edu.au>
2023-09-29 20:55:32 +0800

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { socialLinks } from '../data'
+import SocialLink from './SocialLink'
+import Pagelinks from './Pagelinks'
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <Pagelinks parentClass="footer-links" itemClass="footer-link" />
+
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          return <SocialLink key={link.id} {...link} itemClass="footer-icon" />
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
